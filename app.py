01d0c207e5c66bd2c573faafc67c35a8962eac9f
Subeen Park <psb021414@gmail.com>
2021-12-17 02:11:47 +0900

    server code fininshed

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
+from sentiment import SentimentClassifier
+
+app = Flask(__name__)
+
+classifier = SentimentClassifier()
+
+@app.route('/predict', methods=["POST"])
+def true_or_false():
+    dataRecieve = request.get_json()
+    user_input = dataRecieve["content"]
+
+    return jsonify({
+        'sentiment': f'{classifier.predict(user_input)}',
+    })
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8000)
---
+++ sentiment.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import torch
+from transformers import (ElectraTokenizerFast,
+                          ElectraForSequenceClassification)
+
+
+
+class SentimentClassifier():
+    LABELS = ['분노', '슬픔', '불안', '당황', '상처', '기쁨', ]
+    ID_LABELS = {idx: key for (idx, key) in enumerate(LABELS)}
+    MAX_LEN = 512
+
+    def __init__(self):
+        self.model = ElectraForSequenceClassification.from_pretrained("kykim/electra-kor-base",
+                                                                      problem_type="multi_label_classification",
+                                                                      num_labels=6)
+        self.tokenizer = ElectraTokenizerFast.from_pretrained("kykim/electra-kor-base")
+        self.model.load_state_dict(torch.load("model.pt", map_location=torch.device('cpu')))
+        # print(self.dataset.describe())
+
+    def _get_prediction_input(self, text):
+        inputs = self.tokenizer(
+            text,
+            return_tensors='pt',
+            truncation=True,
+            max_length=SentimentClassifier.MAX_LEN,
+            pad_to_max_length=True,
+            add_special_tokens=True
+        )
+
+        input_ids = inputs['input_ids'][0]
+        attention_mask = inputs['attention_mask'][0]
+
+        return input_ids, attention_mask
+
+    def predict(self, text):
+        input_ids, attention_mask = self._get_prediction_input(text)
+        y_pred = self.model(input_ids.unsqueeze(0), attention_mask=attention_mask.unsqueeze(0))[0]
+        _, predicted = torch.max(y_pred, 1)
+        return SentimentClassifier.ID_LABELS[predicted.item()]
