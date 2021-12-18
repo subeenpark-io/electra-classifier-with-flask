0fdf46d50ac9e3728845e9f6a033591795bf55a9
Subeen Park <psb021414@gmail.com>
2021-12-19 00:21:03 +0900

    keyword api

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 classifier = SentimentClassifier()
 
 @app.route('/sentiment', methods=["POST"])
-def true_or_false():
+def sentiment():
     dataRecieve = request.get_json()
     user_input = dataRecieve["content"]
 
@@ -21,7 +21,9 @@ def ping():
     return "healthy"
 
 @app.route('/keyword')
-def keyword(texts):
+def keyword():
+    dataRecieve = request.get_json()
+    texts = [dataRecieve["text"]]
     keywords, sents = summarize_with_sentences(
         texts,
         diversity=0.5,
@@ -29,6 +31,10 @@ def keyword(texts):
         num_keysents=10,
         verbose=False
     )
+
+    return jsonify({
+        'keywords': keywords,
+    })
 
 
 if __name__=="__main__":
