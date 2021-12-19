db5d4ae1a28fe18c244b252a1dad899c6aedc971
Subeen Park <psb021414@gmail.com>
2021-12-19 12:23:14 +0900

    redefine predict

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 from flask import Flask, request, jsonify
-from sentiment import SentimentClassifier
-from krwordrank.word import summarize_with_keywords
-from krwordrank.hangle import normalize
-from konlpy.tag import Okt
+from sentiment import (SentimentClassifier,
+                        PostKeywordExtractor)
 
 
 app = Flask(__name__)
 
 classifier = SentimentClassifier()
-okt = Okt()
+keyword_extractor = PostKeywordExtractor()
 
-@app.route('/sentiment', methods=["POST"])
-def sentiment():
+
+@app.route('/predict', methods=["POST"])
+def predict_sentiment_and_keywords():
     dataRecieve = request.get_json()
     user_input = dataRecieve["content"]
 
     return jsonify({
         'sentiment': f'{classifier.predict(user_input)}',
+        'keywords': f'{keyword_extractor.extract_keywords(user_input)}',
     })
 
 @app.route('/ping')
@@ -25,41 +25,6 @@ def ping():
     return jsonify({
         'hi': 'HI',
     })
-
-@app.route('/keyword', methods=["POST"])
-def keyword():
-    print(request)
-    dataRecieve = request.get_json()
-    texts = [dataRecieve["text"]]
-    texts = [normalize(text, english=True, number=True) for text in texts]
-
-
-    stopwords = {'너무', '정말', '로만', '원래', '구나', '건데', '돌아', '일단', '사이'}
-
-    try:
-        keywords = summarize_with_keywords(texts, min_count=2, max_length=10,
-                                           beta=0.85, max_iter=10, stopwords=stopwords, verbose=True)
-        keyword_nouns = []
-        for word, r in sorted(keywords.items(), key=lambda x: x[1], reverse=True)[:30]:
-            for noun in okt.nouns(word):
-                if len(noun) > 1:
-                    keyword_nouns.append(noun)
-            print('%8s:\t%.4f' % (word, r))
-
-        if len(keyword_nouns) < 1:
-            raise ValueError("Zero keywords extracted: Fallback to okt_nounts")
-
-        return jsonify({
-            'keywords': keyword_nouns,
-        })
-    except:
-        okt_nouns = okt.nouns(texts[0])
-        print(f"okt_nouns {okt_nouns}")
-        keyword_nouns = [word for word in okt_nouns if len(word) > 1 and word not in stopwords]
-        keyword_nouns = list(set(keyword_nouns))
-        return jsonify({
-            'keywords': keyword_nouns,
-        })
 
 
 if __name__=="__main__":
--- sentiment.py
+++ sentiment.py
@@ -3,9 +3,13 @@ import torch
 from transformers import (ElectraTokenizerFast,
                           ElectraForSequenceClassification)
 
+from krwordrank.word import summarize_with_keywords
+from krwordrank.hangle import normalize
+from konlpy.tag import Okt
 
 
-class SentimentClassifier():
+
+class SentimentClassifier:
     LABELS = ['분노', '슬픔', '불안', '당황', '상처', '기쁨', ]
     ID_LABELS = {idx: key for (idx, key) in enumerate(LABELS)}
     MAX_LEN = 512
@@ -40,3 +44,41 @@ class SentimentClassifier():
         y_pred = self.model(input_ids.unsqueeze(0).to(self.device), attention_mask=attention_mask.unsqueeze(0).to(self.device))[0]
         _, predicted = torch.max(y_pred, 1)
         return SentimentClassifier.ID_LABELS[predicted.item()]
+
+
+class PostKeywordExtractor:
+
+    stopwords = {'너무', '정말', '로만', '원래', '구나', '건데', '돌아', '일단', '사이'}
+
+    def __init__(self):
+        self.okt = Okt()
+        self.okt.nouns("시범 명사")
+
+    def extract_keywords(self, text):
+        texts = [text]
+        texts = [normalize(text, english=True, number=True) for text in texts]
+
+        keyword_nouns = []
+        try:
+            keywords = summarize_with_keywords(texts,
+                                               min_count=2,
+                                               max_length=10,
+                                               beta=0.85,
+                                               max_iter=10,
+                                               stopwords=PostKeywordExtractor.stopwords, verbose=True)
+
+            for word, r in sorted(keywords.items(), key=lambda x: x[1], reverse=True)[:30]:
+                for noun in self.okt.nouns(word):
+                    if len(noun) > 1:
+                        keyword_nouns.append(noun)
+                print('%8s:\t%.4f' % (word, r))
+
+            if len(keyword_nouns) < 1:
+                raise ValueError("Zero keywords extracted: Fallback to okt_nounts")
+            return keyword_nouns
+        except:
+            okt_nouns = self.okt.nouns(texts[0])
+            print(f"okt_nouns {okt_nouns}")
+            keyword_nouns = [word for word in okt_nouns if len(word) > 1 and word not in self.stopwords]
+            keyword_nouns = list(set(keyword_nouns))
+            return keyword_nouns
